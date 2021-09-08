c8db3423b8678eff5f5152028287f9991f5e5e88
Ludvig Hozman <ludvig123@gmail.com>
2021-09-08 10:48:26 +0200

    Add initial fpga/chisel files

---
+++ fpga/src/main/scala/Main.scala
@@ -0,0 +1,42 @@
+import chisel3._
+
+class Main extends Module {
+  val io = IO(new Bundle{
+    val led = Output(Vec(4, UInt(1.W)))
+
+    val btn = Input(Vec(2, UInt(1.W)))
+  })
+
+  var counter = Reg(UInt())
+
+  when(io.btn(0) === true.B) {
+    counter := counter + 1.U
+  }
+  io.led(0) := 0.U
+  io.led(1) := 0.U
+  io.led(2) := 0.U
+  io.led(3) := 0.U
+
+  when(counter === 1.U) {
+    io.led(0) := true.B
+  }
+  when(counter === 2.U) {
+    io.led(2) := true.B
+  }
+  when(counter === 3.U) {
+    io.led(3) := true.B
+  }
+
+  when(counter === 4.U) {
+    counter := 0.U
+    io.led(0) := true.B
+  }
+
+
+  //val x  = Reg(UInt())
+  //val y  = Reg(UInt())
+}
+
+object Main extends App {
+  (new chisel3.stage.ChiselStage).emitVerilog(new Main())
+}
