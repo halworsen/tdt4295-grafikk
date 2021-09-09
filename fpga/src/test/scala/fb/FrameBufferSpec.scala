963572e952a9ebbbe68238e6fa2e7b95551e112f
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-09-09 09:53:36 +0200

    feat: first test, scala formatting
    Also

--- fpga/src/test/scala/fb/FrameBufferSpec.scala
+++ fpga/src/test/scala/fb/FrameBufferSpec.scala
@@ -5,35 +5,17 @@ import chisel3.tester._
 import org.scalatest.FreeSpec
 
 class FrameBufferSpec extends FreeSpec with ChiselScalatestTester {
-  /*
-  "Should initialize correct graphics array without enabling writing" in {
-    test(new FrameBuffer(3, 4)) { dut =>
-      dut.io.writeEnable.poke(false.B)
-      dut.io.selx.poke(1.U)
-      dut.io.sely.poke(1.U)
-      println(dut.io.selected.peek())
-
-      // val matrix = VecInit(Seq.fill(4)(VecInit(Seq.fill(3)(0.U(1.W)))))
-      println(dut.io.writeEnable.peek())
-      // dut.io.output.expect(matrix)
-      println("Horray!")
-    }
-  }
-
-  "Should initialize correct graphics array while enabling writing" in {
-    test(new FrameBuffer(4, 4)) { dut =>
-      dut.io.writeEnable.poke(true.B)
-      val matrix = VecInit(Seq.fill(4)(VecInit(Seq.fill(4)(1.U(1.W)))))
-      dut.io.input.poke(matrix)
-      dut.io.output.expect(dut.io.input)
-    }
-  }*/
-
   "Should initialize correct graphics array while enabling writing" in {
     test(new FrameBuffer(4, 4)) { dut =>
       dut.io.writeX.poke(1.U)
       dut.io.writeY.poke(1.U)
-
+      dut.io.writeEnable.poke(true.B)
+      dut.io.writeVal.poke(true.B)
+      dut.clock.step(1)
+      dut.io.readX.poke(1.U)
+      dut.io.readY.poke(1.U)
+      println(dut.io.readVal.peek())
+      dut.io.readVal.expect(true.B)
     }
   }
 }
