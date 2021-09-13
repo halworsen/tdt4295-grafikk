9c829ddbf0558a6ef140ab05a1f0135f6a57a0ef
Ludvig Hozman <ludvig123@gmail.com>
2021-09-13 12:01:36 +0200

    Finish clock module to adhere to xilinx docs. set to 25.2Mhz

--- fpga/src/main/scala/vga/Clock.scala
+++ fpga/src/main/scala/vga/Clock.scala
@@ -7,20 +7,71 @@ import chisel3._
  *  NOTE: This module needs to be connected to a clock upon usage,
  *  as it is not implicitly connected.
  */
-class MMCE2_BASE(divide_f: Double)
-    extends BlackBox(Map(("CLKIN1_DIVIDE_F" -> divide_f))) {
+class MMCE2_BASE(divide0_f: Double, mult_f: Double = 5)
+    extends BlackBox(
+      Map("CLKOUT0_DIVIDE_F" -> divide0_f, "CLKFBOUT_MULT_F" -> mult_f)
+    ) {
   val io = IO(new Bundle {
-    val clkout0 = Input(UInt(1.W))
+    val CLKIN1 = Input(Clock()) // 1-bit input: Clock input
+    val CLKFBIN = Input(Clock()) // 1-bit input: Feedback clock
+
+    // Control Ports: 1-bit (each) input: MMCM control ports
+    val PWRDWN = Input(Clock()) // 1-bit input: Power-down
+    val RST = Input(Clock()) // 1-bit input: Reset
+
+    // Clock outputs
+    val CLKOUT0 = Output(Clock()) // 1-bit output: CLKOUT0
+    val CLKOUT0B = Output(Clock()) // 1-bit output: CLKOUT0
+    val CLKOUT1 = Output(Clock()) // 1-bit output: CLKOUT1
+    val CLKOUT1B = Output(Clock()) // 1-bit output: Inverted CLKOUT1
+    val CLKOUT2 = Output(Clock()) // 1-bit output: CLKOUT2
+    val CLKOUT2B = Output(Clock()) // 1-bit output: Inverted CLKOUT2
+    val CLKOUT3 = Output(Clock()) // 1-bit output: CLKOUT3
+    val CLKOUT3B = Output(Clock()) // 1-bit output: Inverted CLKOUT3
+    val CLKOUT4 = Output(Clock()) // 1-bit output: CLKOUT4
+    val CLKOUT5 = Output(Clock()) // 1-bit output: CLKOUT5
+    val CLKOUT6 = Output(Clock()) // 1-bit output: CLKOUT6
+
+    // Feedback Clocks: 1-bit (each) output: Clock feedback ports
+    val CLKFBOUT = Output(Clock()) // 1-bit output: Feedback clock
+    val CLKFBOUTB = Output(Clock()) // 1-bit output: Inverted CLKFBOUT
+
+    // Status Ports: 1-bit (each) output: MMCM status ports
+    val LOCKED = Output(Clock()) // 1-bit output: LOCK
   })
 }
 
-class Clock(width: Int, height: Int) extends Module {
+// Wrapper module around ARTIX MMCE2 module
+class Clock() extends Module {
   val io = IO(new Bundle {
-    val clkout = Output(UInt(1.W))
+    val clkout = Output(Clock())
   })
 
-  val MMCE2_BASE_inst = Module(new MMCE2_BASE(3.972))
-  io.clkout := MMCE2_BASE_inst.io.clkout0
+  val MMCE2_BASE_inst = Module(new MMCE2_BASE(7.944, 2)) // 25.17 MHz
+
+  // If needed and the synthesizer does not add a buffer, we can use the builtin BUFG module to add
+  // a clock buffer here and connect between the output of this module and the MMCE2
+  // BUFG bufg_clk(.I(clokout_unbuffered), .O(clkout));
+
+  MMCE2_BASE_inst.io.CLKIN1 := clock
+  MMCE2_BASE_inst.io.RST := reset
+  MMCE2_BASE_inst.io.CLKFBIN := DontCare
+
+  io.clkout := MMCE2_BASE_inst.io.CLKOUT0
+
+  MMCE2_BASE_inst.io.CLKOUT0B := DontCare
+  MMCE2_BASE_inst.io.CLKOUT1 := DontCare
+  MMCE2_BASE_inst.io.CLKOUT1B := DontCare
+  MMCE2_BASE_inst.io.CLKOUT2 := DontCare
+  MMCE2_BASE_inst.io.CLKOUT2B := DontCare
+  MMCE2_BASE_inst.io.CLKOUT3 := DontCare
+  MMCE2_BASE_inst.io.CLKOUT3B := DontCare
+  MMCE2_BASE_inst.io.CLKOUT4 := DontCare
+  MMCE2_BASE_inst.io.CLKOUT5 := DontCare
+  MMCE2_BASE_inst.io.CLKOUT6 := DontCare
+
+  MMCE2_BASE_inst.io.PWRDWN := DontCare
+
 }
 
 /*
