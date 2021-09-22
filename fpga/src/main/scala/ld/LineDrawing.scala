230032f58f8d6fb9058556a3b3ca05172ab869cb
Ludvig <ludvig123@gmail.com>
2021-09-22 13:40:53 +0200

    Connect clock,vga, framebuffer and linedrawer (#14)
    * Connect clock and vga module
    
    * Reroute FB and linedrawer module using IO and try make it compile
    
    * Connect the fb and linedrawer properly
    
    * Update LineDrawing.scala
    
    Co-authored-by: Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>

--- fpga/src/main/scala/ld/LineDrawing.scala
+++ fpga/src/main/scala/ld/LineDrawing.scala
@@ -1,11 +1,10 @@
 package ld
 
 import chisel3._
-import fb.FrameBuffer
-import tools.helpers.{log2, setPixel}
+import chisel3.util._
+import tools.helpers.log2
 
 class LineDrawing(
-    fb: FrameBuffer,
     xStart: Int,
     xEnd: Int,
     yStart: Int,
@@ -16,13 +15,32 @@ class LineDrawing(
     val ys = Input(UInt(log2(yStart).W))
     val xe = Input(UInt(log2(xEnd).W))
     val ye = Input(UInt(log2(yEnd).W))
+
+    // This is needed instead of setPixel
+    val writeEnable = Output(Bool())
+    val writeX = Output(UInt(log2Up(xEnd).W))
+    val writeY = Output(UInt(log2Up(yEnd).W))
+    val writeVal = Output(Bool())
+
     val done = Output(RegInit(true.B))
   })
+
   val x = Reg(UInt(log2(xStart).W))
   val y = Reg(UInt(log2(yStart).W))
   val dx = Reg(SInt(log2(xEnd).W))
   val dy = Reg(SInt(log2(yEnd).W))
   val e = Reg(SInt(log2(xStart).W))
+
+  val writeEnable = RegInit(false.B)
+  val writeX = Reg(UInt(log2(xEnd).W))
+  val writeY = Reg(UInt(log2(yEnd).W))
+  val writeVal = RegInit(true.B)
+
+  x := io.xs
+  y := io.ys
+  dx := io.xe - io.xs
+  dy := io.ye - io.ys
+  e := -(dx >> 1.U).asSInt();
 
   when(io.done) {
     // Initialize registers
@@ -33,10 +51,14 @@ class LineDrawing(
     dy := io.ye - io.ys
     e := -(dx >> 1.U).asSInt();
   }
-
   when(x <= io.xe) {
     // Draw pixel
-    setPixel(fb, x.asUInt(), y.asUInt(), true.B)
+    writeEnable := true.B
+    writeX := x.asUInt()
+    writeY := y.asUInt()
+    writeVal := true.B
+
+    //setPixel(enable, x.asUInt(), y.asUInt(), true.B)
     x := x + 1.U;
     e := e + dy;
     when(e >= 0.S) {
@@ -45,5 +67,9 @@ class LineDrawing(
     }
   }.otherwise {
     io.done := true.B
+    io.writeEnable := writeEnable
+    io.writeX := writeX
+    io.writeY := writeY
+    io.writeVal := writeVal
   }
 }
