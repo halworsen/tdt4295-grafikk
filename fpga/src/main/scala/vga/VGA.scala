322344538a799fe3d2251261eafae89c6537854c
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-09-29 09:33:43 +0200

    Update VGA.scala

--- fpga/src/main/scala/vga/VGA.scala
+++ fpga/src/main/scala/vga/VGA.scala
@@ -25,7 +25,7 @@ class VGA extends Module {
     val (counterHsync, counterHsyncWrap) = Counter(0 to 800)
     val (counterVsync, counterVsyncWrap) = Counter(counterHsyncWrap, 525)
 
-    io.hsync := counterHsync >= (640 + 16).U & counterHsync < (640 + 16 + 96).U 
+    io.hsync := counterHsync >= (640 + 16).U & counterHsync < (640 + 16 + 96).U
     io.vsync := counterVsync >= (480 + 10).U & counterVsync < (480 + 10 + 2).U
     io.out := counterHsync < 640.U & counterVsync < 480.U
     io.enable := counterHsync < 640.U & counterVsync < 480.U
