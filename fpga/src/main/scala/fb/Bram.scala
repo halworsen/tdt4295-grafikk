7d5824e8f11f9c40c3ff37b7429445a6da656518
Ludvig Hozman <ludvig123@gmail.com>
2021-10-06 14:04:41 +0200

    Use standard memory instead ov xilinx macro.
    Make the framebuffer work! This adds a simple 640x480 framebuffer and
    connects this to the VGA module. Also adds some example memory buffers
    to load the framebuffer with sample images.

--- fpga/src/main/scala/fb/Bram.scala
+++ fpga/src/main/scala/fb/Bram.scala
@@ -1,6 +1,7 @@
 package fb
 
 import chisel3._
+import chisel3.util._
 import chisel3.util.HasBlackBoxResource
 
 // Wrapper module around ARTIX BRAM_SDP_MACRO
@@ -8,14 +9,39 @@ class Bram extends BlackBox with HasBlackBoxResource {
   val io = IO(new Bundle {
     val clk_read = Input(Clock())
     val clk_write = Input(Clock())
+    val reset = Input(Reset())
     val read_enable = Input(Bool())
     val write_enable = Input(Bool())
+    val read_out_reg_en = Input(Bool())
+    val writeport_enable = Input(Bool())
 
-    val read_addr = Input(UInt(10.W))
-    val write_addr = Input(UInt(10.W))
+    val read_addr = Input(UInt(20.W))
+    val write_addr = Input(UInt(15.W))
 
-    val data_in = Input(UInt(12.W))
-    val data_out = Output(UInt(12.W))
+    val data_in = Input(Bits(12.W))
+    //val data_out = Output(Bits(12.W))
+    val data_out = Output(Bits(1.W))
   })
   addResource("/bram_sdp.sv")
+}
+
+class Bram_sdp(width: Int, depth: Int, init_file: String = "")
+    extends BlackBox(
+      Map("WIDTH" -> width, "DEPTH" -> depth, "INIT_F" -> init_file)
+    )
+    with HasBlackBoxResource {
+  val io = IO(new Bundle {
+    val clk_read = Input(Clock())
+    val clk_write = Input(Clock())
+    val write_enable = Input(Bool())
+
+    val write_addr = Input(UInt(log2Up(depth).W))
+    val read_addr = Input(UInt(log2Up(depth).W))
+
+    val data_in = Input(Bits(width.W))
+    //val data_out = Output(Bits(12.W))
+    val data_out = Output(Bits(width.W))
+  })
+
+  addResource("/bram_sdp_custom.sv")
 }
--- fpga/src/main/scala/vga/VGA.scala
+++ fpga/src/main/scala/vga/VGA.scala
@@ -9,13 +9,12 @@ class VGA extends Module {
    * We primaraly focus on a 640 x 480px implementation
    */
   val io = IO(new Bundle {
-    // Data is for now just a bool
     val data = Input(Vec(3, UInt(4.W)))
+    val dataEnable = Output(Bool()) // Data enable. We can write to pixel.
     val selX = Output(UInt(16.W))
     val selY = Output(UInt(16.W))
     val hsync = Output(Bool())
     val vsync = Output(Bool())
-    val enable = Output(Bool()) // Data enable. We can write to pixel.
     val out = Output(Vec(3, UInt(4.W)))
 
     val clock = Input(Clock())
@@ -28,20 +27,27 @@ class VGA extends Module {
 
     io.hsync := counterHsync >= (640 + 16).U & counterHsync < (640 + 16 + 96).U
     io.vsync := counterVsync >= (480 + 10).U & counterVsync < (480 + 10 + 2).U
-    io.out := counterHsync < 640.U & counterVsync < 480.U
-    io.enable := counterHsync < 640.U & counterVsync < 480.U
+    io.dataEnable := counterHsync < 640.U & counterVsync < 480.U
 
     io.selX := counterHsync
     io.selY := counterVsync
 
-    when(~io.enable) {
-      io.selX := 0.U
-      io.selY := 0.U
-      io.out := io.data
+    when(~io.dataEnable) {
+      io.data := DontCare
+      io.out(0) := "h0".U
+      io.out(1) := "h0".U
+      io.out(2) := "h0".U
     }.otherwise {
-      io.selX := counterHsync
-      io.selY := counterVsync
-      io.out := "h0".U
+      //io.out := io.data
+      when(io.data(0) === 1.U) {
+        io.out(0) := "hC".U
+        io.out(1) := "h9".U
+        io.out(2) := "h1".U
+      } otherwise {
+        io.out(0) := "h1".U
+        io.out(1) := "h1".U
+        io.out(2) := "h1".U
+      }
     }
   }
 }
