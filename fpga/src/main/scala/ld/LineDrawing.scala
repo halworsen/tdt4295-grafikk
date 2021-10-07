aa401be26d4c533d9ea2d134203cc553a6293933
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-10-07 13:44:54 +0200

    We swap on x coordinates

--- fpga/src/main/scala/ld/LineDrawing.scala
+++ fpga/src/main/scala/ld/LineDrawing.scala
@@ -2,8 +2,8 @@ package ld
 
 import chisel3._
 import chisel3.util._
-import tools.helpers.log2
 
+// https://www.google.com/search?q=bresenham%27s+line+algorithm&sxsrf=AOaemvL1TxCiXvAjdJEoqqyHit-YOLiabQ:1633605785405&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwjQtqrkl7jzAhXnQvEDHVxHC0AQ_AUoAXoECAEQAw&biw=1440&bih=788&dpr=2#imgrc=MemqMsI7g2nbiM
 class LineDrawing(
     xStart: Int,
     xEnd: Int,
@@ -11,10 +11,10 @@ class LineDrawing(
     yEnd: Int
 ) extends Module {
   val io = IO(new Bundle {
-    val xs = Input(UInt(log2(xStart).W))
-    val ys = Input(UInt(log2(yStart).W))
-    val xe = Input(UInt(log2(xEnd).W))
-    val ye = Input(UInt(log2(yEnd).W))
+    val xs = Input(UInt(log2Up(xStart).W))
+    val ys = Input(UInt(log2Up(yStart).W))
+    val xe = Input(UInt(log2Up(xEnd).W))
+    val ye = Input(UInt(log2Up(yEnd).W))
     // Todo: should we have an input that controls whether or not we should output? (output enable)
 
     // This is needed instead of setPixel
@@ -26,29 +26,28 @@ class LineDrawing(
     val done = Output(RegInit(true.B))
   })
 
-  val x = Reg(UInt(log2(xStart).W)) // Drawing position
-  val y = Reg(UInt(log2(yStart).W)) // Drawing position
-  val dx = Reg(SInt(log2(xEnd).W))
-  val dy = Reg(SInt(log2(yEnd).W))
-  val e = Reg(SInt(log2(xStart).W)) // Error value
+  val x = Reg(UInt(log2Up(xStart).W)) // Drawing position
+  val y = Reg(UInt(log2Up(yStart).W)) // Drawing position
+  val dx = Reg(SInt(log2Up(xEnd).W))
+  val dy = Reg(SInt(log2Up(yEnd).W))
+  val e = Reg(SInt(log2Up(xStart).W)) // Error value
 
   val writeEnable = RegInit(false.B)
-  val writeX = Reg(UInt(log2(xEnd).W))
-  val writeY = Reg(UInt(log2(yEnd).W))
+  val writeX = Reg(UInt(log2Up(xEnd).W))
+  val writeY = Reg(UInt(log2Up(yEnd).W))
   val writeVal = RegInit(true.B)
 
   // Needed because Bresenham's algorithm is not
-  // symmertical, and we don't want gaps
+  // symmetrical, and we don't want gaps
   val xa = Wire(UInt())
   val xb = Wire(UInt())
   val ya = Wire(UInt())
   val yb = Wire(UInt())
 
-  // Swap points if y_start is less than y_end to ensure
-  // a consistent way to order the points. By swapping
-  // y-coordinates if y_start is greater than y_end, we
-  // will ensure that we always draw down ont he screen.
-  when(io.ys < io.ye) {
+  // Swap points if x_start is less than x_end to ensure
+  // a consistent way to order the points. This way
+  // ensures that we always draw down on the screen.
+  when(io.xs < io.xe) {
     xa := io.xs;
     xb := io.xe;
     ya := io.ys;
@@ -65,7 +64,7 @@ class LineDrawing(
     io.done := false.B
     x := xa
     y := ya
-    dx := xb - xa // todo: må vi ha absoluttverdi her?
+    dx := xb - xa
     dy := yb - ya
     e := -(dx >> 1.U).asSInt();
   }
@@ -76,7 +75,6 @@ class LineDrawing(
     writeY := y.asUInt()
     writeVal := true.B
 
-    //setPixel(enable, x.asUInt(), y.asUInt(), true.B)
     x := x + 1.U;
     e := e + dy;
     when(e >= 0.S) {
