c928cbec24839e0d3dc86ae698b51dd8a2c93661
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-10-07 13:59:48 +0200

    remove bram class

--- fpga/src/main/scala/fb/Bram.scala
+++ fpga/src/main/scala/fb/Bram.scala
@@ -2,28 +2,6 @@ package fb
 
 import chisel3._
 import chisel3.util._
-import chisel3.util.HasBlackBoxResource
-
-// Wrapper module around ARTIX BRAM_SDP_MACRO
-class Bram extends BlackBox with HasBlackBoxResource {
-  val io = IO(new Bundle {
-    val clk_read = Input(Clock())
-    val clk_write = Input(Clock())
-    val reset = Input(Reset())
-    val read_enable = Input(Bool())
-    val write_enable = Input(Bool())
-    val read_out_reg_en = Input(Bool())
-    val writeport_enable = Input(Bool())
-
-    val read_addr = Input(UInt(20.W))
-    val write_addr = Input(UInt(15.W))
-
-    val data_in = Input(Bits(12.W))
-    //val data_out = Output(Bits(12.W))
-    val data_out = Output(Bits(1.W))
-  })
-  addResource("/bram_sdp.sv")
-}
 
 class Bram_sdp(width: Int, depth: Int, init_file: String = "")
     extends BlackBox(
