7fef069dc4b653fc936a1b54df193484bf161409
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-10-07 13:29:49 +0200

    Update Matrix.scala

--- fpga/src/main/scala/matrix/Matrix.scala
+++ fpga/src/main/scala/matrix/Matrix.scala
@@ -4,10 +4,10 @@ import chisel3._
 
 class MVP extends Module {
   val io = IO(new Bundle {
-    val vec4 = Input(Vec(4, 32.W))
-    val mat4 = Input(Vec(4, Vec(4, 32.W)))
-    val outVec4 = Output(Vec(4, 32.W))
-  }
+    val vec4 = Input(Vec(4, SInt(32.W)))
+    val mat4 = Input(Vec(4, Vec(4, SInt(32.W))))
+    val outVec4 = Output(Vec(4, SInt(32.W)))
+  })
 
   for (r <- 0 to 3) {
     for (c <- 0 to 3) {
@@ -18,16 +18,16 @@ class MVP extends Module {
 
 class Mat4Multiply extends Module {
   val io = IO(new Bundle {
-    val vec4 = Input(Vec(4, 32.W))
-    val mat4 = Input(Vec(4, Vec(4, 32.W)))
-    val outMat4 = Output(Vec(4, Vec(4, 32.W)))
-  }
+    val A = Input(Vec(4, Vec(4, SInt(32.W))))
+    val B = Input(Vec(4, Vec(4, SInt(32.W))))
+    val C = Output(Vec(4, Vec(4, SInt(32.W))))
+  })
 
   for (r <- 0 to 3) {
     for (c <- 0 to 3) {
-      io.outMat4(r) := 0.S
+      io.C(r) := 0.S
       for (k <- 0 to 3) {
-        io.outMat4(r) := io.outMat4(r) + (io.mat4(r)(c) * io.vec4(c))
+        io.C(r)(c) := io.A(r)(k) * io.B(k)(c)
       }
     }
   }
