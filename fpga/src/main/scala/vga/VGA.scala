b7e049b7db3c376c8e17317cfe6a073972f2c06d
Ludvig Hozman <ludvig123@gmail.com>
2021-10-13 13:21:09 +0200

    Revert "Framebuffer (#23)"
    This reverts commit 0c4a14a2f97e3fc215c1c88c53ebe78f934a105f.

--- fpga/src/main/scala/vga/VGA.scala
+++ fpga/src/main/scala/vga/VGA.scala
@@ -5,17 +5,17 @@ import chisel3.util.Counter
 
 class VGA extends Module {
 
-  /*
-   * We primaraly focus on a 640 x 480px implementation
-   */
+  /** We primaraly focus on a 640 x 480px implementation
+    */
   val io = IO(new Bundle {
-    val data = Input(Vec(3, UInt(4.W)))
-    val dataEnable = Output(Bool()) // Data enable. We can write to pixel.
+    // Data is for now just a bool
+    val data = Input(Bool())
     val selX = Output(UInt(16.W))
     val selY = Output(UInt(16.W))
     val hsync = Output(Bool())
     val vsync = Output(Bool())
-    val out = Output(Vec(3, UInt(4.W)))
+    val enable = Output(Bool()) // Data enable. We can write to pixel.
+    val out = Output(Bool())
 
     val clock = Input(Clock())
     val reset = Input(Bool())
@@ -27,27 +27,10 @@ class VGA extends Module {
 
     io.hsync := counterHsync >= (640 + 16).U & counterHsync < (640 + 16 + 96).U
     io.vsync := counterVsync >= (480 + 10).U & counterVsync < (480 + 10 + 2).U
-    io.dataEnable := counterHsync < 640.U & counterVsync < 480.U
+    io.out := counterHsync < 640.U & counterVsync < 480.U
+    io.enable := counterHsync < 640.U & counterVsync < 480.U
 
     io.selX := counterHsync
     io.selY := counterVsync
-
-    when(~io.dataEnable) {
-      io.data := DontCare
-      io.out(0) := "h0".U
-      io.out(1) := "h0".U
-      io.out(2) := "h0".U
-    }.otherwise {
-      //io.out := io.data
-      when(io.data(0) === 1.U) {
-        io.out(0) := "hC".U
-        io.out(1) := "h9".U
-        io.out(2) := "h1".U
-      } otherwise {
-        io.out(0) := "h1".U
-        io.out(1) := "h1".U
-        io.out(2) := "h1".U
-      }
-    }
   }
 }
--- fpga/src/test/scala/fb/FrameBufferSpec.scala
+++ fpga/src/test/scala/fb/FrameBufferSpec.scala
@@ -10,16 +10,11 @@ class FrameBufferSpec extends FreeSpec with ChiselScalatestTester {
       dut.io.writeX.poke(1.U)
       dut.io.writeY.poke(1.U)
       dut.io.writeEnable.poke(true.B)
-      dut.io.writeVal(0).poke(1.U)
-      dut.io.writeVal(1).poke(1.U)
-      dut.io.writeVal(2).poke(1.U)
+      dut.io.writeVal.poke(true.B)
       dut.clock.step(1)
       dut.io.readX.poke(1.U)
       dut.io.readY.poke(1.U)
-      dut.clock.step(1)
-    //dut.io.readVal(0).expect(1.U)
-    //dut.io.readVal(1).expect(0.U)
-    //dut.io.readVal(2).expect(0.U)
+      dut.io.readVal.expect(true.B)
     }
     println("Success!")
   }
