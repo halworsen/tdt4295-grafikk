27c72e64ea960d55bfdcf8e15455da8396e04915
Jafagervik <jorgenafagervik@gmail.com>
2021-10-19 22:58:36 +0200

    Update TriangleDrawer.scala

--- fpga/src/main/scala/drawers/TriangleDrawer.scala
+++ fpga/src/main/scala/drawers/TriangleDrawer.scala
@@ -2,6 +2,7 @@ package fb
 
 import chisel3._
 import chisel3.util._
+import ld.LineDrawing
 
 class TriangleDrawer() extends Module {
 
@@ -12,6 +13,11 @@ class TriangleDrawer() extends Module {
     val triangle = Output();
   })
 
-  
+  /*
+  - connect to framebuffer
+  - Set up bresenham 3 times
+  - let framebuffer draw
+
+  */
   
 }
