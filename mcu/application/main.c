19b6c8a03630c89eec74f1e2b8d6673f55bbc46e
erlingfn <erling.nesset@gmail.com>
2021-10-20 17:59:41 +0200

    Add function for calculating points

--- mcu/application/main.c
+++ mcu/application/main.c
@@ -11,21 +11,53 @@
 #include "spidrv.h"
 #include "timer.h"
 
-struct mat4 {
-  float data[16];
-};
+#define MAX_SAMPLE 4096
 
-struct vect {
-  float x;
-  float y;
-  float z;
-  float w;
-};
+#define DISPLAY_HEIGHT 480
+#define DISPLAY_WIDTH 640
+
+#define MIN_X 40
+#define MIN_Y 60
+#define MAX_X 440
+#define MAX_Y 600
+
+#define max(a, b)                                                              \
+  ({                                                                           \
+    __typeof__(a) _a = (a);                                                    \
+    __typeof__(b) _b = (b);                                                    \
+    _a > _b ? _a : _b;                                                         \
+  })
+
+#define min(a, b)                                                              \
+  ({                                                                           \
+    __typeof__(a) _a = (a);                                                    \
+    __typeof__(b) _b = (b);                                                    \
+    _a < _b ? _a : _b;                                                         \
+  })
 
 uint32_t ch1_sample;
 uint32_t ch2_sample;
 uint32_t adcChannel = 0;
 ADC_InitSingle_TypeDef initSingle = ADC_INITSINGLE_DEFAULT;
+
+void calc_points(uint32_t ch1_sample, uint32_t ch2_sample, int *coordinates) {
+  double scale;
+  int x;
+  int y;
+
+  scale = (double)ch1_sample / MAX_SAMPLE;
+  x = scale * MAX_X;
+  x = min(x, MAX_X);
+  x = max(x, MIN_X);
+
+  scale = (double)ch2_sample / MAX_SAMPLE;
+  y = scale * MAX_Y;
+  y = min(y, MAX_Y);
+  y = max(y, MIN_Y);
+
+  coordinates[0] = x;
+  coordinates[1] = y;
+}
 
 void GPIO_EVEN_IRQHandler(void) {
   GPIO_IntClear(0x5555);
@@ -33,6 +65,8 @@ void GPIO_EVEN_IRQHandler(void) {
 }
 
 void TIMER1_IRQHandler(void) {
+  int coordinates[2] = {MIN_X, MIN_Y};
+
   TIMER_IntClear(TIMER1, 1);
   if (adcChannel)
     initSingle.input = adcSingleInputCh0;
@@ -41,6 +75,7 @@ void TIMER1_IRQHandler(void) {
   ADC_InitSingle(ADC0, &initSingle);
   ADC_Start(ADC0, adcStartSingle);
   adcChannel = !adcChannel;
+  calc_points(ch1_sample, ch2_sample, coordinates);
 }
 
 void ADC0_IRQHandler(void) {
@@ -50,16 +85,6 @@ void ADC0_IRQHandler(void) {
     ch1_sample = sample;
   else
     ch2_sample = sample;
-
-  if (ch1_sample > 2000)
-    GPIO_PinOutSet(BSP_GPIO_LED1_PORT, BSP_GPIO_LED1_PIN);
-  else
-    GPIO_PinOutClear(BSP_GPIO_LED1_PORT, BSP_GPIO_LED1_PIN);
-
-  if (ch2_sample > 2000)
-    GPIO_PinOutSet(BSP_GPIO_LED0_PORT, BSP_GPIO_LED0_PIN);
-  else
-    GPIO_PinOutClear(BSP_GPIO_LED0_PORT, BSP_GPIO_LED0_PIN);
 }
 
 int main(void) {
