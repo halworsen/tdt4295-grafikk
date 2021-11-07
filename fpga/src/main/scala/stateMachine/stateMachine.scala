139b65f3c2c582cb49559dca99c71206b4015dfd
Eirik <eirikw@live.no>
2021-11-07 04:07:41 +0100

    IT WORKS

--- fpga/src/main/scala/stateMachine/stateMachine.scala
+++ fpga/src/main/scala/stateMachine/stateMachine.scala
@@ -7,55 +7,63 @@ import tools._
 class StateMachine extends Module {
   val io = IO(new Bundle {
     val newFrameRecieved = Input(Bool())
-    val bhDone = Input(Bool())
+    val bhBussy = Input(Bool())
 
     val bhStartRegular = Output(Bool())
     val bhStartClear = Output(Bool())
     val loadNextFrame = Output(Bool())
-    val lineIndex = Output(UInt())
+    val lineIndex = Output(UInt(STD.linenum.W))
   })
 
   io.bhStartRegular := false.B
   io.bhStartClear := false.B
   io.loadNextFrame := false.B
 
-  val lineIndex = RegInit(0.U)
+  val lineIndex = RegInit(0.U(STD.linenum.W))
   io.lineIndex := lineIndex
   val unrenderedFrame = RegInit(false.B)
   when(io.newFrameRecieved) {
     unrenderedFrame := true.B
   }
 
-  val waiting :: clearing :: rendering :: Nil = Enum(3)
+  val waiting :: clearSetup :: clearing ::renderSetup :: rendering :: Nil = Enum(5)
   val state = RegInit(waiting)
 
   switch(state) {
     is(waiting) {
       when(unrenderedFrame) {
         lineIndex := 0.U
+        state := clearSetup
+      }
+    }
+    is(clearSetup){
+        // Wait 1 cycle for bh inputs to become valid
         io.bhStartClear := true.B
         state := clearing
-      }
     }
     is(clearing) {
-      when(io.bhDone && lineIndex < STD.linenum.U) {
+      when(!io.bhBussy && lineIndex < STD.linenum.U) {
         lineIndex := lineIndex + 1.U
-        io.bhStartClear := true.B
+        state := clearSetup
       }
-      when(io.bhDone && lineIndex === STD.linenum.U) {
+      when(!io.bhBussy && lineIndex >= STD.linenum.U) {
         io.loadNextFrame := true.B
         unrenderedFrame := false.B
-        io.bhStartRegular := true.B
         lineIndex := 0.U
-        state := rendering
+        state := renderSetup
       }
     }
+    is(renderSetup){
+        // Wait 1 cycle for bh inputs to become valid
+        io.bhStartRegular := true.B
+        state := rendering
+    }
     is(rendering) {
-      when(io.bhDone && lineIndex < STD.linenum.U) {
+      when(!io.bhBussy && lineIndex < STD.linenum.U) {
         lineIndex := lineIndex + 1.U
-        io.bhStartClear := true.B
+        state := renderSetup
       }
-      when(io.bhDone && lineIndex === STD.linenum.U) {
+      when(!io.bhBussy && lineIndex >= STD.linenum.U) {
         state := waiting
       }
     }
