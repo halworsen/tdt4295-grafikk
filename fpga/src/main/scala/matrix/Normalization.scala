f94e698074d3f1ff127dcca0cca235e6a9c57a82
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-11-09 10:16:54 +0100

    Update Normalization.scala

--- fpga/src/main/scala/matrix/Normalization.scala
+++ fpga/src/main/scala/matrix/Normalization.scala
@@ -14,9 +14,9 @@ class Normalizer extends Module {
 
   val tempZ := divFP(io.point.z, io.point.w)
 
-  // io.pixel.x := divFP(divFP(io.point.x, io.point.w), tempZ)
-  // io.pixel.y := divFP(divFP(io.point.y, io.point.w), tempZ)  
+  val pixelX := divFP(divFP(io.point.x, io.point.w), tempZ)
+  val pixelY := divFP(divFP(io.point.y, io.point.w), tempZ)  
 
-  io.pixel.x := ((divFP(io.point.x, io.point.w) + toFP(1.S)).pad(32) * toFP(STD.screenWidth.S).pad(32)).head(32-STD.binaryPoint)
-  io.pixel.y := ((divFP(io.point.y, io.point.w) + toFP(1.S)).pad(32) * -toFP(STD.screenHeight.S).pad(32)).head(32-STD.binaryPoint)
+  io.pixel.x := ((pixelX + toFP(1.S)).pad(32) * toFP(STD.screenWidth.S).pad(32)).head(32-STD.binaryPoint)
+  io.pixel.y := ((pixelY + toFP(1.S)).pad(32) * -toFP(STD.screenHeight.S).pad(32)).head(32-STD.binaryPoint)
 }
