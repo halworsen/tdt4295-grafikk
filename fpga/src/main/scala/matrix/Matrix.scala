ef3a0af6c695027074251c185076cc62910be4a9
Eirik <eirikw@live.no>
2021-11-09 09:38:12 +0100

    Added w normalization and point to pixel translation

--- fpga/src/main/scala/matrix/Matrix.scala
+++ fpga/src/main/scala/matrix/Matrix.scala
@@ -1,26 +1,32 @@
 package matrix
 
 import chisel3._
+import chisel3.experimental.FixedPoint
+import tools._
+
 
 class MVP extends Module {
   val io = IO(new Bundle {
-    val vec4 = Input(Vec(4, SInt(32.W)))
-    val mat4 = Input(Vec(4, Vec(4, SInt(32.W))))
-    val outVec4 = Output(Vec(4, SInt(32.W)))
+    val vec4 = Input(new Point)
+    val mat4 = Input(Vec(4, Vec(4, FixedPoint(STD.fixedWidth, STD.binaryPoint.BP))))
+    val outVec4 = Output(new Point)
   })
 
-  for (r <- 0 to 3) {
-    io.outVec4(r) := (io.mat4(r)(0) * io.vec4(0) + io.mat4(r)(1) * io.vec4(
-      1
-    ) + io.mat4(r)(2) * io.vec4(2) + io.mat4(r)(3) * io.vec4(3))
-  }
+  io.outVec4.x := (io.mat4(0)(0) * io.vec4.x + io.mat4(0)(1) 
+                  * io.vec4.y + io.mat4(0)(2) * io.vec4.z + io.mat4(0)(3) * io.vec4.w)
+  io.outVec4.y := (io.mat4(1)(0) * io.vec4.x + io.mat4(1)(1) 
+                  * io.vec4.y + io.mat4(1)(2) * io.vec4.z + io.mat4(1)(3) * io.vec4.w)
+  io.outVec4.z := (io.mat4(2)(0) * io.vec4.x + io.mat4(2)(1) 
+                  * io.vec4.y + io.mat4(2)(2) * io.vec4.z + io.mat4(2)(3) * io.vec4.w)
+  io.outVec4.w := (io.mat4(3)(0) * io.vec4.x + io.mat4(3)(1) 
+                  * io.vec4.y + io.mat4(3)(2) * io.vec4.z + io.mat4(3)(3) * io.vec4.w)
 }
 
 class Mat4Multiply extends Module {
   val io = IO(new Bundle {
-    val A = Input(Vec(4, Vec(4, SInt(32.W))))
-    val B = Input(Vec(4, Vec(4, SInt(32.W))))
-    val C = Output(Vec(4, Vec(4, SInt(32.W))))
+    val A = Input(Vec(4, Vec(4, FixedPoint(STD.fixedWidth, STD.binaryPoint.BP))))
+    val B = Input(Vec(4, Vec(4, FixedPoint(STD.fixedWidth, STD.binaryPoint.BP))))
+    val C = Output(Vec(4, Vec(4, FixedPoint(STD.fixedWidth, STD.binaryPoint.BP))))
   })
 
   for (r <- 0 to 3) {
--- fpga/src/main/scala/tools/helpers.scala
+++ fpga/src/main/scala/tools/helpers.scala
@@ -3,9 +3,19 @@ package tools
 import chisel3._
 
 import scala.math.log10
+import chisel3.experimental.FixedPoint
 
-object helpers {
-  var log2 = (x: Int) => (log10(x) / log10(2.0)).toInt
+object Helpers {
+  var log2 = (x: Int) => (log10(x) / log10(2.0)).toInt : Int
+  val toFP = (v: SInt) => (v << STD.binaryPoint).asFixedPoint(STD.binaryPoint.BP) : FixedPoint
+  val divFP = (a: FixedPoint, w: FixedPoint) => ((a.asUInt()<<STD.binaryPoint) / w.asUInt()).asFixedPoint(STD.binaryPoint.BP)
+
+  /** Generate a chisel FixedPoiny from a decimal number (double)
+    *
+    * @return 
+    *   The newly created fixedpoint
+    */
+  val decToFP = (v: Double) => FixedPoint.fromDouble(v, STD.fixedWidth, STD.binaryPoint.BP) : FixedPoint
 
   def bitNr(reg: UInt, nr: Int, bufferWidth: Int = 64, dataWidth: Int = 16) = {
     reg(
