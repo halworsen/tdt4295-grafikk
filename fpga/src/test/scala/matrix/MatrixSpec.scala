10ba3b2ce7d4bf5da990a255acaca37d12b98e54
Ludvig Hozman <ludvig123@gmail.com>
2021-11-19 11:33:45 +0100

    Edit matrix tests to work with FP
    Adds `dsptools` as this is the preferred way to test with FP and
    floating point numbers. The DspTester includes tolerances.

--- fpga/src/test/scala/matrix/MatrixSpec.scala
+++ fpga/src/test/scala/matrix/MatrixSpec.scala
@@ -1,8 +1,10 @@
 package matrix
 
+import scala.math.pow
 import chisel3._
 import chisel3.experimental._
 import chisel3.tester._
+import dsptools._
 import org.scalatest.FreeSpec
 import tools.helpers.fp
 
@@ -17,37 +19,33 @@ class MVPSpec extends FreeSpec with ChiselScalatestTester {
         dut.io.mat4(3)(i).poke(fp(1))
       }
 
-      dut.io.outVec4(0).expect(fp(1.375))
-      dut.io.outVec4(1).expect(fp(1.375))
-      dut.io.outVec4(2).expect(fp(1.375))
-      dut.io.outVec4(3).expect(fp(1.375))
+      dut.io.outVec4(0).expect(fp(1))
+      dut.io.outVec4(1).expect(fp(0.5))
+      dut.io.outVec4(2).expect(fp(2))
+      dut.io.outVec4(3).expect(fp(2))
     }
     println("Successfully multiplied 4x1 with 4x4 matrix!")
   }
 }
 
-class Mat4MultiplySpec extends FreeSpec with ChiselScalatestTester {
-  "Should multiply 4x4 with 4x4" in {
-    test(new Mat4Multiply) { dut =>
-      for (i <- 0 to 3) {
-        dut.io.A(0)(i).poke(fp(i + 1))
-        dut.io.A(1)(i).poke(fp(i + 1))
-        dut.io.A(2)(i).poke(fp(i + 1))
-        dut.io.A(3)(i).poke(fp(i + 1))
+class Mat4MultiplySpec(dut: Mat4Multiply) extends DspTester(dut) {
+  for (i <- 0 to 3) {
+    dut.io.A(0)(i).poke(fp(pow(2, -i)))
+    dut.io.A(1)(i).poke(fp(pow(2, -i)))
+    dut.io.A(2)(i).poke(fp(pow(2, -i)))
+    dut.io.A(3)(i).poke(fp(pow(2, -i)))
 
-        dut.io.B(0)(i).poke(fp(i + 1))
-        dut.io.B(1)(i).poke(fp(i + 1))
-        dut.io.B(2)(i).poke(fp(i + 1))
-        dut.io.B(3)(i).poke(fp(i + 1))
-      }
-
-      for (j <- 0 to 3) {
-        dut.io.C(j)(0).expect(fp(10))
-        dut.io.C(j)(1).expect(fp(20))
-        dut.io.C(j)(2).expect(fp(30))
-        dut.io.C(j)(3).expect(fp(40))
-      }
-    }
-    println("Successfully multiplied 4x4 with 4x4 matrix!")
+    dut.io.B(0)(i).poke(fp(0.2 - i / 10))
+    dut.io.B(1)(i).poke(fp(0.2 - i / 10))
+    dut.io.B(2)(i).poke(fp(0.2 - i / 10))
+    dut.io.B(3)(i).poke(fp(0.2 - i / 10))
   }
+
+  for (j <- 0 to 3) {
+    dut.io.C(j)(0).expect(fp(0.375))
+    dut.io.C(j)(1).expect(fp(0.1875))
+    dut.io.C(j)(2).expect(fp(0))
+    dut.io.C(j)(3).expect(fp(-0.1873))
+  }
+  println("Successfully multiplied 4x4 with 4x4 matrix!")
 }
