9f2c05a9471e38cdc3015af09491fd0832db1e7d
Ludvig Hozman <ludvig123@gmail.com>
2021-11-20 21:04:47 +0100

    Calculate correct fb address & only switch buffer when needed
    The `current_buffer` bit was optimized out by chisel, due to too large
    bit widths. So the buffer was never switched for reads. This also fixes
    ghosting issued caused by always switching buffer, without clearing it.
    This will switch buffer only when clearing.

--- fpga/src/main/scala/fb/FrameBuffer.scala
+++ fpga/src/main/scala/fb/FrameBuffer.scala
@@ -9,13 +9,14 @@ class FrameBuffer(width: Int, height: Int) extends Module {
 
   val colorDepth: Int = 4 // Bit width of a single color channel
   val pixel_count = width * height
+  val buf_width = math.pow(2, log2Up(pixel_count)).toInt
 
   val io = IO(new Bundle {
     val writePixel = Input(new Pixel)
     val writeVal = Input(Bool())
     val writeEnable = Input(Bool())
-    val readX = Input(UInt((log2Up(width) + 1).W))
-    val readY = Input(UInt((log2Up(height) + 1).W))
+    val readX = Input(UInt(log2Up(width).W))
+    val readY = Input(UInt(log2Up(height).W))
     val readVal = Output(Bool())
     val readEnable = Input(Bool())
     val clear = Input(Bool())
@@ -28,30 +29,26 @@ class FrameBuffer(width: Int, height: Int) extends Module {
 
   io.clearStarted := false.B
 
-  val frame_done = Module(new CDC)
-  frame_done.io.clk_in := io.readClock
-  frame_done.io.clk_out := clock
-  frame_done.io.input := io.frameDone
-  val frameDone = frame_done.io.output ^ RegNext(frame_done.io.output)
+  val frameDone = io.frameDone
 
   val fb_internal =
     Module(
       new Bram_sdp(
         1,
-        2 * math.pow(2, log2Up(pixel_count)).toInt /*, "./bugge_large.mem"*/
+        2 * buf_width /*, "./bugge_large.mem"*/
       )
     )
 
   val init :: clear :: idle :: active :: Nil = Enum(4)
   val state = RegInit(idle)
-  val fb_clear_addr = RegInit(0.U(log2Up(pixel_count).W))
-  val fb_write_addr = RegInit(0.U(log2Up(pixel_count).W))
+  val fb_clear_addr = RegInit(0.U(log2Up(buf_width).W))
+  val fb_write_addr = RegInit(0.U(log2Up(buf_width).W))
   val current_buffer = RegInit(0.U)
   val write_val = Wire(Bool())
 
   io.ready := state === active
 
-  when(frameDone) {
+  when(frameDone && io.clear) {
     current_buffer := ~current_buffer
   }
 
@@ -69,8 +66,9 @@ class FrameBuffer(width: Int, height: Int) extends Module {
     is(clear) {
       when(fb_clear_addr === (pixel_count - 1).U) {
         state := active
+      }.otherwise {
+        fb_clear_addr := fb_clear_addr + 1.U
       }
-      fb_clear_addr := fb_clear_addr + 1.U
     }
 
     is(idle) {
@@ -94,7 +92,8 @@ class FrameBuffer(width: Int, height: Int) extends Module {
     write_val := io.writeVal
   }
 
-  val out_of_frame = io.writePixel.x > width.U || io.writePixel.y > height.U
+  val out_of_frame =
+    io.writePixel.x >= width.U || io.writePixel.x < 0.U || io.writePixel.y >= height.U || io.writePixel.y < 0.U
 
   fb_internal.io.clk_write := clock
   //fb.io.reset := reset
@@ -102,27 +101,19 @@ class FrameBuffer(width: Int, height: Int) extends Module {
   //fb.io.read_enable := io.readEnable
 
   fb_internal.io.write_enable := RegNext(
-    io.writeEnable && !out_of_frame
+    io.writeEnable && !out_of_frame && io.ready
   ) || (state === clear)
-  fb_internal.io.write_addr := Cat(current_buffer, fb_write_addr)
+  fb_internal.io.write_addr := Cat(~current_buffer, fb_write_addr)
   fb_internal.io.data_in := write_val.asUInt()
 
   //fb.io.writeport_enable := DontCare
   //fb.io.read_out_reg_en := true.B
 
   withClock(io.readClock) {
-    val read_addr = RegInit(0.U(20.W))
+    val read_addr = Wire(UInt(log2Up(buf_width).W))
+    read_addr := io.readY * width.U + io.readX
     fb_internal.io.clk_read := io.readClock
-    fb_internal.io.read_addr := Cat(~current_buffer, read_addr)
-    when(io.readY === 0.U && io.readX === 0.U) {
-      read_addr := 0.U
-    }
-      .elsewhen(io.readY < height.U && io.readX < width.U) {
-        read_addr := read_addr + 1.U
-      }
-    //r(0) := fb.io.data_out(colorDepth - 1, 0)
-    //r(1) := fb.io.data_out(2 * colorDepth - 1, colorDepth)
-    //r(2) := fb.io.data_out(3 * colorDepth - 1, 2 * colorDepth)
+    fb_internal.io.read_addr := Cat(current_buffer, read_addr)
   }
 
   io.readVal := fb_internal.io.data_out.asBool()
